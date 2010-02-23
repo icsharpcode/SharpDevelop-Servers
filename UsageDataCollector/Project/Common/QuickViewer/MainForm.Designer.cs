d0c0332b81bcc509be7305155c7c11de5a4113f3
Christoph Wille <christophw@alphasierrapapa.com>
2010-02-23 14:44:33 +0000

    Sample data for usage data collector

---
+++ UsageDataCollector/Project/Common/QuickViewer/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace QuickViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileContents = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FileContents
+            // 
+            this.FileContents.AllowDrop = true;
+            this.FileContents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileContents.Location = new System.Drawing.Point(0, 0);
+            this.FileContents.Multiline = true;
+            this.FileContents.Name = "FileContents";
+            this.FileContents.ReadOnly = true;
+            this.FileContents.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.FileContents.Size = new System.Drawing.Size(650, 386);
+            this.FileContents.TabIndex = 0;
+            this.FileContents.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.FileContents.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 386);
+            this.Controls.Add(this.FileContents);
+            this.Name = "MainForm";
+            this.Text = "Xml.Gz Quick Viewer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FileContents;
+    }
+}
+
---
+++ UsageDataCollector/Project/Common/QuickViewer/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ICSharpCode.UsageDataCollector.ServiceLibrary.Import;
+using ICSharpCode.UsageDataCollector.Contracts;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace QuickViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] fileDrop = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (null != fileDrop)
+                {
+                    string filename = fileDrop.GetValue(0).ToString();
+
+                    UsageDataMessage currentMessage = FileImporter.ReadMessage(filename);
+
+                    DataContractSerializer dataContractSerializer = new DataContractSerializer(typeof(UsageDataMessage));
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        dataContractSerializer.WriteObject(memoryStream, currentMessage);
+                        byte[] data = new byte[memoryStream.Length];
+                        Array.Copy(memoryStream.GetBuffer(), data, data.Length);
+                        string message = Encoding.UTF8.GetString(data);
+
+                        FileContents.Text = message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                FileContents.Text = ex.ToString();
+            }
+        }
+
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+
+        }
+    }
+}
