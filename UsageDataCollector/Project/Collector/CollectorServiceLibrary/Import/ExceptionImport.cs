fe9cebe7337992f6432a35ad1320938eb5e98c6a
Christoph Wille <christophw@alphasierrapapa.com>
2010-02-25 13:02:41 +0000

    Modify ExceptionImport to store all data denormalised

--- UsageDataCollector/Project/Collector/CollectorServiceLibrary/Import/ExceptionImport.cs
+++ UsageDataCollector/Project/Collector/CollectorServiceLibrary/Import/ExceptionImport.cs
@@ -2,68 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ICSharpCode.UsageDataCollector.Contracts;
 
 namespace ICSharpCode.UsageDataCollector.ServiceLibrary.Import
 {
     public class ExceptionImport
     {
-        public ExceptionImport(string extype)
+        public string Fingerprint { get; private set; }
+        public string Type { get; private set; }
+        public string StackTrace { get; private set; }
+        public string Location { get; private set; }
+        public DateTime Time { get; private set; }
+
+        public ExceptionImport(UsageDataException ude)
+            : this(ude.ExceptionType, ude.StackTrace, ude.Time)
         {
         }
 
-        public string Fingerprint
+        public ExceptionImport(string ExceptionType, string StackTrace, DateTime Time)
         {
-            get
-            {
-                return this.Type + Environment.NewLine +
-                    string.Join(
-                        Environment.NewLine,
-                        ExceptionHelpers.CleanStackTrace(
-                            ExceptionHelpers.SplitLines(this.StackTrace)
-                        ));
-            }
+            this.Type = Type;
+            this.StackTrace = StackTrace;
+            this.Time = Time;
+
+            CalculateFingerprint();
+            CalculateLocation();
+
+            //TODO: calculate fingerprint hash
         }
 
-        public string StackTrace { get; set; }
-
-        public string Type
+        private void CalculateFingerprint()
         {
-            get
-            {
-                return ExceptionHelpers.SplitLines(this.Fingerprint).First();
-            }
+            this.Fingerprint = this.Type + Environment.NewLine +
+                string.Join(
+                    Environment.NewLine,
+                    ExceptionHelpers.CleanStackTrace(
+                        ExceptionHelpers.SplitLines(this.StackTrace)
+                    ));
         }
 
         static readonly Type[] argumentExceptions = { typeof(ArgumentException), typeof(ArgumentNullException), typeof(ArgumentOutOfRangeException) };
 
-        public string Location
+        private void CalculateLocation()
         {
-            get
-            {
-                List<string> stackTrace = ExceptionHelpers.SplitLines(this.Fingerprint).Skip(1).ToList();
-                // ignore any ThrowHelper (etc.) methods at the top of the stack
-                if (stackTrace.Count > 0 && GetFunctionName(stackTrace[0]).Contains("Throw"))
-                    stackTrace.RemoveAt(0);
+            List<string> stackTrace = ExceptionHelpers.SplitLines(this.Fingerprint).Skip(1).ToList();
+            // ignore any ThrowHelper (etc.) methods at the top of the stack
+            if (stackTrace.Count > 0 && GetFunctionName(stackTrace[0]).Contains("Throw"))
+                stackTrace.RemoveAt(0);
 
-                if (stackTrace.Count == 0)
-                    return "unknown";
-                string type = this.Type;
-                if (argumentExceptions.Any(e => e.FullName == type) && ExceptionHelpers.IsUserCode(stackTrace[0]))
-                {
-                    // find first stack frame supplying the invalid argument
-                    string functionName = GetFunctionName(stackTrace[0]);
-                    string result = stackTrace.FirstOrDefault(l => GetFunctionName(l) != functionName);
-                    // report it if it's user code
-                    if (result != null && ExceptionHelpers.IsUserCode(result))
-                        return result;
-                    else
-                        return stackTrace[0];
-                }
+            if (stackTrace.Count == 0)
+                this.Location = "unknown";
+
+            string type = this.Type;
+            if (argumentExceptions.Any(e => e.FullName == type) && ExceptionHelpers.IsUserCode(stackTrace[0]))
+            {
+                // find first stack frame supplying the invalid argument
+                string functionName = GetFunctionName(stackTrace[0]);
+                string result = stackTrace.FirstOrDefault(l => GetFunctionName(l) != functionName);
+                // report it if it's user code
+                if (result != null && ExceptionHelpers.IsUserCode(result))
+                    this.Location = result;
                 else
-                {
-                    // report first user-code stack frame
-                    return stackTrace.FirstOrDefault(ExceptionHelpers.IsUserCode) ?? stackTrace[0];
-                }
+                    this.Location = stackTrace[0];
+            }
+            else
+            {
+                // report first user-code stack frame
+                this.Location = stackTrace.FirstOrDefault(ExceptionHelpers.IsUserCode) ?? stackTrace[0];
             }
         }
 
