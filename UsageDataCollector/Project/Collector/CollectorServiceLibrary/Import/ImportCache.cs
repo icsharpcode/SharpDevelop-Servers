9c4fa3a8de0bd84c2b6696613d97bbc74be8ea23
Christoph Wille <christophw@alphasierrapapa.com>
2010-02-26 11:57:37 +0000

    Notes for implementation, skeleton for Cache

--- UsageDataCollector/Project/Collector/CollectorServiceLibrary/Import/ImportCache.cs
+++ UsageDataCollector/Project/Collector/CollectorServiceLibrary/Import/ImportCache.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Web.Caching;
+
+using ICSharpCode.UsageDataCollector.DataAccess.Collector;
+using System.Web;
+
 namespace ICSharpCode.UsageDataCollector.ServiceLibrary.Import
 {
-    // TODO: write import cache shim to improve speed
-    public class ImportCache
+    public sealed class ImportCache
     {
+        public void ImplementCacheLater()
+        {
+            // HttpRuntime.Cache
+        }
+
+        // Singleton members
+        ImportCache()
+        {
+        }
+
+        public static ImportCache Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly ImportCache instance = new ImportCache();
+        }
     }
+
 }
