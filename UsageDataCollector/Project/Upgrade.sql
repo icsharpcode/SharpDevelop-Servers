e7234a21ee8c96ac6a7b6417226174a16ea31736
Daniel Grunwald <daniel@danielgrunwald.de>
2012-02-19 21:09:00 +0100

    Add stored procedure

--- UsageDataCollector/Project/Upgrade.sql
+++ UsageDataCollector/Project/Upgrade.sql
@@ -53,3 +53,51 @@ BEGIN
 END
 
 GO
+
+/****** Object:  StoredProcedure [dbo].[FeatureIndex]    Script Date: 02/19/2012 21:07:58 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Daniel Grunwald
+-- Create date: 19.02.2012
+-- Description:	Index of Feature Uses
+-- =============================================
+CREATE PROCEDURE [dbo].[FeatureIndex]
+	-- Add the parameters for the stored procedure here
+	@featureNamePattern varchar(255),
+	@commitID int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT
+		[Features].FeatureId, FeatureName, TotalUseCount, UserDays
+	FROM (
+		SELECT
+			FeatureId,
+			SUM(UseCount) AS TotalUseCount,
+			COUNT(1) AS UserDays
+		FROM (
+			SELECT FeatureUse.FeatureId AS FeatureId, COUNT(1) as UseCount
+			FROM FeatureUse
+			INNER JOIN [Sessions] ON [Sessions].SessionId = FeatureUse.SessionId
+			INNER JOIN Features ON [Features].FeatureId = FeatureUse.FeatureId
+			WHERE [Sessions].CommitId = @commitID
+			  AND FeatureName LIKE @featureNamePattern
+			GROUP BY UserId, DATEADD(day, DATEDIFF(day,0,StartTime), 0), FeatureUse.FeatureId
+		) AS X
+		GROUP BY FeatureId
+	) As Y
+	INNER JOIN [Features] ON [Features].FeatureId = Y.FeatureId
+	ORDER BY UserDays DESC, TotalUseCount DESC
+END
+
+GO
+
